a3dd61c7bb4e718d6cad485181e2a7031502be92
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-06-27 21:36:22 +0700

    Time: 0 ms (100%), Space: 18.4 MB (66.7%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode *head = new ListNode();
-        ListNode *ans = head;
-        int len = lists.size();
 
-        while(true){
-            bool empty = true;
+    ListNode *merge(ListNode *l1, ListNode *l2){
+        if(!l1) return l2;
+        if(!l2) return l1;
 
-            for(ListNode* x : lists){
-                if(x) empty = false;
-            }
-            if(empty) break;
-
-            int lowestIdx = 0, lowestVal = INT_MAX;
-            for(int i=0; i<len; i++){
-                if(lists[i] && lowestVal > lists[i]->val){
-                    lowestVal = lists[i]->val;
-                    lowestIdx = i;
-                }
-            }
-
-            ans->next = lists[lowestIdx];
-            lists[lowestIdx] = lists[lowestIdx]->next;
-            ans = ans->next;
-            ans->next = nullptr;
-
+        if(l1->val < l2->val){
+            l1->next = merge(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = merge(l1, l2->next);
+            return l2;
         }
+    }
 
-        return head->next;
+    ListNode *divide(vector<ListNode*>& lists, int left, int right){
+        if(left == right) return lists[left];
+        int mid = left + (right-left)/2;
+        
+        ListNode *l1 = divide(lists, left, mid);
+        ListNode *l2 = divide(lists, mid+1, right);
+
+        return merge(l1, l2);
+        
+
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()) return nullptr;
+        int len = lists.size();
+        return divide(lists, 0 ,len-1);
     }
 };
