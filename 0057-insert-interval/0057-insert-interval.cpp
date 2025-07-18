14e9a598e615872d38a3d00caa4844fa473c7a50
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-07-18 10:49:35 +0700

    Time: 2 ms (34.99%), Space: 21.8 MB (45.06%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,50 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> ans;
-        
-        vector<pair<int,int>> indexed;
-        int size = intervals.size();
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
 
-        indexed.push_back({newInterval[0],-1});
-        indexed.push_back({newInterval[1],-1});
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
 
-        for(int i=0;i<size;i++){
-            indexed.push_back({intervals[i][0], i});
-            indexed.push_back({intervals[i][1], i});
-        }
-
-        sort(indexed.begin(), indexed.end(), [](auto& a, auto& b) {
-            return a.first < b.first;
-        });
-
-        unordered_map<int,int> indexQ;
-        vector<int> dq;
-        
-        for(pair<int,int> &val: indexed){
-            indexQ[val.second] += 1;
-            dq.push_back(val.first);
-            bool check = true;
-            for(auto it = indexQ.begin(); it != indexQ.end(); ++it){
-                if(it->second != 2){
-                    check = false;
-                    break;
-                }
+        for(auto it=intervals.begin(); it != intervals.end(); ++it){
+            vector<int> &pair= *it;
+            if(res.back()[1] >= pair[0]){
+                res.back()[1] = max(pair[1], res.back()[1]);
+            }else{
+                res.push_back(pair);
             }
-
-            if(check){
-                indexQ.clear();
-                int size = ans.size();
-                if(size > 0 && ans[size-1][1] == dq.front()){
-                    ans[size-1][1] = dq.back();
-                }else{
-                    ans.push_back({dq.front(), dq.back()});
-                }
-                dq.clear();
-            }
-            
         }
-
-        return ans;
+        return res;
     }
 };
