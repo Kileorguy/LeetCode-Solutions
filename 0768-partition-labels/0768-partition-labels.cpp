0f780305f443cf4608ffda27229903272dff9e51
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-07-27 20:43:27 +0700

    Time: 0 ms (100%), Space: 8.9 MB (71.81%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char, int> startMap;
         unordered_map<char,int> endMap;
         int len = s.length();
         char c;
         for(int i=0;i<len;i++){
             c = s[i];
-            if(startMap.count(c) == 0){
-                startMap[c] = i;
-            }
             endMap[c] = i;
         }
-        queue<char> q;
         vector<int> ans;
-        int count = 1;
+        int start = 0, end = 0;
         for(int i=0;i<len;i++){
             c = s[i];
-            if(startMap[c] == i) q.push(c);
-            if(endMap[c] == i) q.pop();
-            if(!q.empty()) count++;
-            else{
-                ans.push_back(count);
-                count = 1;
+            end = max(end, endMap[c]);
+            if(end == i){
+                ans.push_back(end-start+1);
+                start = i+1;
             }
         }
         return ans;
