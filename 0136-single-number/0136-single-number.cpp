e79eee81331533b0d1e152c3a460b3f7adaa21ae
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-08-19 21:43:57 +0700

    Time: 0 ms (100%), Space: 20.7 MB (58.12%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> us;
-        for(int &n :nums){
-            if(us.count(n) == 0){
-                us.insert(n);
-            }else{
-                us.erase(n);
-            }
+        int result = 0;
+        for (int n : nums) {
+            result ^= n;
         }
-        for ( auto it = us.begin(); it != us.end(); ++it ) return *it;
-        return -1;
+        return result;
     }
 };
