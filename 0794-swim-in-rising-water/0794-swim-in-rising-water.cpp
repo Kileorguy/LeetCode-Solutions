3de66551753621a1985d46d58f89a383c17325e0
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-10-02 18:28:41 +0700

    Time: 7 ms (62.23%), Space: 14.3 MB (39.31%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    int h[4] = {0,1,0,-1};
-    int v[4] = {1,0,-1,0};
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         vector<vector<bool>> vis(n, vector<bool>(n, false));
 
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        using T = tuple<int,int,int>;
+        priority_queue<T, vector<T>, greater<T>> pq;
 
-        // weight, i, j
-        pq.push({grid[0][0],0,0});
+        pq.push({grid[0][0], 0, 0});
+        int ans = 0;
 
-        vector<int> curr = pq.top();
-        int weight = curr[0],i=curr[1],j=curr[2];
+        int dirs[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
 
-        int ans = weight;
+        while (!pq.empty()) {
+            auto [weight, i, j] = pq.top(); pq.pop();
 
-        while (i!=n-1 || j!=n-1) {
-            curr = pq.top(); pq.pop();
-            weight = curr[0];
+            if (vis[i][j]) continue;
+            vis[i][j] = true;
+
             ans = max(ans, weight);
-            i=curr[1], j=curr[2];
 
-            vis [i][j] = true;
+            if (i == n-1 && j == n-1) return ans;
 
-            for (int a=0; a < 4; a++) {
-                int ii = i + v[a];
-                int jj = j + h[a];
-
-                if (validate(ii,jj,n) && !vis[ii][jj]) {
-                    pq.push({grid[ii][jj],ii,jj});
+            for (auto& d : dirs) {
+                int ii = i + d[0];
+                int jj = j + d[1];
+                if (validate(ii, jj, n) && !vis[ii][jj]) {
+                    pq.push({grid[ii][jj], ii, jj});
                 }
             }
         }
-        return ans;
+        return ans; 
     }
 private:
-    bool validate(int &i, int &j, int &n) {
+    bool validate(int i, int j, int n) {
         return i >= 0 && i < n && j >= 0 && j < n;
     }
 };
