03416b28848540a0cc34c0735fa3af04831b8149
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-12-09 09:57:56 +0700

    Time: 167 ms (34.95%), Space: 65 MB (6.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
+    struct pair_hash {
+        template<typename T1, typename T2>
+        std::size_t operator()(const std::pair<T1,T2>& p) const {
+            return std::hash<T1>{}(p.first) ^ (std::hash<T2>{}(p.second) << 1);
+        }
+    };
 
-    void dfs(vector<int> &nums, int &target, int &ans, int &sum, int idx) {
-        for (int i=idx; i<nums.size();i++) {
-            sum -= nums[i] * 2;
-            if (sum == target) ans++;
-            dfs(nums, target,ans, sum, i+1);
-            sum += nums[i] * 2;
+    unordered_map<pair<int,int>, int, pair_hash> um;
+
+    int dfs(vector<int> &nums, int target, int idx, int currSum){
+        
+        if(um.count({idx,currSum})>0){
+            return um[{idx,currSum}];
         }
 
-        return;
+        if(idx < 0){
+            if(currSum == target) return 1;
+            return 0;
+        }
+
+        int positive = dfs(nums, target, idx-1, currSum + nums[idx]);
+        int negative = dfs(nums, target, idx-1, currSum - nums[idx]);
+
+        um[{idx, currSum}] = positive+negative; 
+
+        return um[{idx, currSum}];
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = 0;
-        int sum = 0;
-        for (const int n: nums) {
-            sum+=n;
-        }
-        if (target == sum) ans++;
-
-        dfs(nums, target, ans, sum, 0);
-
-        return ans;
+        return dfs(nums, target, nums.size()-1, 0);
     }
 };
