11e5a83c669a7bd6e72e4d4de834093938ccb287
Dionisius Hendi Krisnanto <123861865+Kileorguy@users.noreply.github.com>
2025-12-26 17:00:37 +0700

    Time: 3 ms (91.95%), Space: 13 MB (73.22%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+int minDistance(string word1, string word2) {
+        // if(word1.length() > word2.length()){
+        //     string temp = word1;
+        //     word1 = word2; 
+        //     word2 = temp;
+        // }
+
+
+        vector<vector<int>> dp(word1.length()+1, vector<int>(word2.length()+1, 0));
+
+        for(int i=1;i<=word2.length();i++){
+            dp[0][i] = i;
+        }
+        for(int i=1;i<=word1.length();i++){
+            dp[i][0] = i;
+        }
+
+        for(int i=1;i<=word1.length();i++){
+            for(int j=1;j<=word2.length();j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1+ min(min(dp[i-1][j-1], dp[i][j-1]), dp[i-1][j]);
+                }
+            }
+        }
+
+        
+
+        // for(int i=0;i<=word1.length();i++){
+        //     for(int j=0;j<=word2.length();j++){
+        //         if(dp[i][j] == INT_MAX) cout << "- ";
+        //         else cout << dp[i][j] << " ";
+        //     }
+        //     cout << " " << endl;
+        // }
+        return dp[word1.length()][word2.length()];
+    }
+};
